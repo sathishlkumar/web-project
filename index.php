e61d101762f04b8f21f6c1d01c51b3fd3e2d0327
sathishlkumar <sathishkumarpvr.it18@bitsathy.ac.in>
2021-11-13 17:34:59 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8" />
+    <script src="script.js"></script>
+    <title> Countryside </tirle>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+  </head>
+  <body>
+    <nav class="menuItems">
+      <ol>
+        <li><a href="#"><img src="./images/homepage.jpg" alt="homepageImage.jpg"/></a></li>
+        <li><a href="products.php">Products</a></li>
+        <li><a href="feedback.php">Feedback</a></li>
+        <li><a href="about.php">About</a></li>
+      </ol>
+    </nav>
+    <h1>Welcome to the site exclusive for farmers and consumers</h1>
+    
+    <div class="mainPageEndSection">
+    <footer>
+    <p>Contact the author of this page:</p>
+    <address>
+      <span>ManojKumar N</span>
+      <p> Organization: Countryside INC </p>
+      <a href="mailto:manojkumarn@gmail.com">jim@rock.com</a><br>
+      <a href="tel:+919788117076">(311) 555-2368</a>
+      <p>You may also want to visit us:<br>1/140,Sathyamangalam,Erode,TN </p>
+      If you see any bugs, please <a  href="mailto:manojkumarn@gmail.com">
+    contact webmaster</a>
+    </address>
+    </footer>
+    </div>
+  </body>
+</html>
+<?php
+echo "hello";
+?>
